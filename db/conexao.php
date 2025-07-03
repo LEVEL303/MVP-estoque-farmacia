9288f8fcf1bd33881089d2f943c52d3a9f14199d
Levitico Rimon <leviticorimon@gmail.com>
2025-07-02 21:21:11 -0300

    feat: adicionar conexão com o banco de dados

---
+++ db/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'estoque_farmacia';
+
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conexao->connect_error) {
+    die('Erro na conexão: ' . $conexao->connect_error);
+}
